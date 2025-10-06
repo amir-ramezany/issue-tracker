cfb626bc0a436ac2c4e612b8927ea0916150e15c
amir_ramezani <ramezaniamir47@gmail.com>
2025-10-06 16:58:39 +0330

    add simple mardowneditor to new issuepage

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import React from "react";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 const newIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title">
         <TextField.Slot />
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
